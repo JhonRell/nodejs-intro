baa20051bb178ccc216aeac955b30e7a8620db75
JhonRell <124646784+JhonRell@users.noreply.github.com>
2023-04-18 11:17:06 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,14 @@
 const http = require('http');
 
 const hostname = '127.0.0.1';
-const port = 1999;
+const port = 3000;
 
-const server = http.createServer
-((req, res)=>
-{    
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Jhon Rell Gutang\n');
-}
-);
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('JHON RELL GUTANG\n');
+});
 
-server.listen
-(
-    port, hostname, () => 
-    {
-        console.log('Server running at http://${hostname}:${port}/');
-    }
-); 
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
